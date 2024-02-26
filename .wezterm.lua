773d1a8d432e2d298a83a6a08e49ec9fa3a94a82
XDeme <fernando.tagawa.gamail.com@gmail.com>
2024-02-26 19:38:54 -0300

    Add zsh config

--- .wezterm.lua
+++ .wezterm.lua
@@ -13,11 +13,12 @@ config.hide_tab_bar_if_only_one_tab = true
 config.warn_about_missing_glyphs = false
 
 config.window_close_confirmation = "NeverPrompt"
-config.window_background_opacity = 0.8
+config.window_background_opacity = 0.9
 config.window_padding = {
     left = 0,
     right = 0,
     top = 0,
     bottom = 0,
 }
+
 return config
